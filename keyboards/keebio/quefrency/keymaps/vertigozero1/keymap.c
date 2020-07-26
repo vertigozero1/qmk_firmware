2fe889f94202bdaa62efc764de08fbf67edf1cb1
JS <25235048+vertigozero1@users.noreply.github.com>
2020-07-26 15:50:47 -0500

    moved unicode

--- keyboards/keebio/quefrency/keymaps/vertigozero1/keymap.c
+++ keyboards/keebio/quefrency/keymaps/vertigozero1/keymap.c
@@ -1,5 +1,22 @@
 #include QMK_KEYBOARD_H
 
+enum unicode_names {
+    DEGREE,
+    INTERROBANG,
+    IRONY,
+    INVBANG,
+    PREGUNTA,
+    SNEK
+};
+
+const uint32_t PROGMEM unicode_map[] = {
+    [DEGREE] = 0x00B0,   // °
+    [INTERROBANG]  = 0x203D,  // ‽
+    [IRONY] = 0x2E2E,    // ⸮
+    [INVBANG] = 0x00A1,  // ¡
+    [PREGUNTA] = 0x00BF, // ¿
+    [SNEK]  = 0x1F40D,   // 🐍
+};
 
 enum custom_keycodes {
   QWERTY = SAFE_RANGE,
