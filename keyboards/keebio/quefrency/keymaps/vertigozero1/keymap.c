9dc7904ef9f382be62b1b158d8eaf4a37357e725
JS <25235048+vertigozero1@users.noreply.github.com>
2020-07-26 15:04:30 -0500

    Update keymap.c

--- keyboards/keebio/quefrency/keymaps/vertigozero1/keymap.c
+++ keyboards/keebio/quefrency/keymaps/vertigozero1/keymap.c
@@ -56,6 +56,7 @@ void encoder_update_user(uint8_t index, bool clockwise) {
     }
 }
 
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	[0] = LAYOUT_65_with_macro(SHRUG,  KC_CALC, KC_GESC, KC_1,    KC_2,    KC_3,  KC_4,   KC_5,  KC_6,   KC_7,     KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC, KC_BSPC, KC_MUTE, 
 							   KC_F1,  KC_F2,   KC_TAB,  KC_Q,    KC_W,    KC_E,  KC_R,   KC_T,  KC_Y,   KC_U,     KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, KC_PAUS, 
